5e07fa1954c8acf910e32a25e45507bc4fd00ea4
Gustavo Rojas <56005777+GustavoRojasFlores@users.noreply.github.com>
2021-05-04 12:05:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,7 @@ function calculate () {
   const a = document.getElementById('a').value * 1
   const b = document.getElementById('b').value * 1
   const h = document.getElementById('h').value * 1
-
   const calc = ((a + b) / 2) * h
-  
   if (calc * 0 === 0) {
     document.getElementById('output').innerHTML = calc
   } else {
